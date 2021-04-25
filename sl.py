9d75ba21a1028fc25603221b433735daa8929626
Nicholas K Warfield <nick@nkwarfield.io>
2021-04-25 13:36:51 -0700

    Get Selection Function
    Created a function to grab and sanitize input from the user. I've decided to just use a random input if the user input is invalid.

--- sl.py
+++ sl.py
@@ -1,4 +1,24 @@
-import subprocess
+#!/usr/bin/python3
 
-def main:
-    pass
+import sys
+import subprocess
+import random
+
+def get_selection(lower_bound, upper_bound) -> int:
+    selection = random.randint(lower_bound, upper_bound)
+    if len(sys.argv) == 3 and sys.argv[1] == '-n':
+        try:
+            temp = int(sys.argv[2])
+            if temp >= lower_bound and temp <= upper_bound:
+                selection = temp
+        except:
+            pass
+
+    return selection
+
+selection = get_selection(0, 2)
+
+#subprocess.run(["./train_animation"])
+print(selection)
+
+
